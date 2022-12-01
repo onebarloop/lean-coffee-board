bd2e55efbcdf011abb63a9a29488b74d26e17e1b
Alexander Bayer <alexanderbayer@posteo.de>
2022-12-01 11:42:51 +0100

    Add basic styling

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,14 +1,31 @@
+import styled from "styled-components";
+
 import { nanoid } from "nanoid";
 export default function Card({ entries }) {
   return (
-    <ul>
+    <StyledList>
       {entries.map((entry) => (
-        <li key={nanoid()}>
+        <StyledListItem key={nanoid()}>
           {" "}
           <p>{entry.thoughts}</p>
           <p>{entry.name}</p>
-        </li>
+        </StyledListItem>
       ))}
-    </ul>
+    </StyledList>
   );
 }
+
+const StyledList = styled.ul`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  padding: 5rem;
+  list-style: none;
+  padding: 5rem;
+  margin: auto;
+`;
+
+const StyledListItem = styled.li`
+  border: 3px solid black;
+  padding: 1rem;
+`;
--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 export default function Form({ onNewEntry }) {
   const submit = (event) => {
     event.preventDefault();
@@ -5,17 +7,73 @@ export default function Form({ onNewEntry }) {
       name: event.target.elements.name.value,
       thoughts: event.target.elements.thoughts.value,
     });
+    event.target.reset();
   };
 
   return (
-    <form onSubmit={submit}>
-      <label htmlFor="thoughts">Your Thoughts</label>
-      <textarea id="thoughts" name="thoughts"></textarea>
-      <label htmlFor="name" name="name">
-        Your Name
-      </label>
-      <input id="name" name="name>"></input>
-      <button>Submit</button>
-    </form>
+    <StyledFieldset>
+      <legend>Submit your thoughts</legend>
+      <StyledForm onSubmit={submit}>
+        <label htmlFor="thoughts">Your Thoughts</label>
+        <textarea id="thoughts" name="thoughts" required></textarea>
+        <label htmlFor="name" name="name">
+          Your Name
+        </label>
+        <input id="name" name="name" required></input>
+        <StyledSubmitBtn>Submit</StyledSubmitBtn>
+      </StyledForm>
+    </StyledFieldset>
   );
 }
+
+const StyledFieldset = styled.fieldset`
+  padding: 2rem;
+  width: 70%;
+  margin: auto;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  input {
+    width: 30%;
+  }
+`;
+
+const StyledSubmitBtn = styled.button`
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  border: 2px solid #e74c3c;
+  border-radius: 0.6em;
+  color: #e74c3c;
+  cursor: pointer;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-align-self: center;
+  -ms-flex-item-align: center;
+  align-self: center;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1;
+
+  padding: 1.2em 2.8em;
+  text-decoration: none;
+  text-align: center;
+  text-transform: uppercase;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 700;
+  -webkit-transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
+  transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
+  &:hover {
+    color: #fff;
+    outline: 0;
+    box-shadow: 0 0 40px 40px #e74c3c inset;
+  }
+`;
